0a90553aa94f2b3c7a6513c35f7b9b1bff2a8663
waldrumpus <waldrumpus@gmail.com>
2024-05-30 09:18:44 +0200

    initial commit

---
+++ EventCentral.cs
@@ -0,0 +1,81 @@
+namespace Events;
+
+public sealed class EventCentral
+{
+    private static readonly Lazy<EventCentral> _instance = new(() => new EventCentral());
+    public static EventCentral Instance => _instance.Value;
+    private EventCentral() { }
+
+    private readonly Dictionary<Type, List<object>> _registry = new();
+
+    public void Subscribe<TEvent>(Action<TEvent> handler)
+    {
+        if (!_registry.TryGetValue(typeof(TEvent), out var handlers))
+        {
+            handlers = [];
+            _registry.Add(typeof(TEvent), handlers);
+        }
+
+        handlers.Add(handler);
+    }
+
+    public void SubscribeAsync<TEvent>(Func<TEvent, Task> handler)
+    {
+        if (!_registry.TryGetValue(typeof(TEvent), out var handlers))
+        {
+            handlers = new List<object>();
+            _registry.Add(typeof(TEvent), handlers);
+        }
+
+        handlers.Add(handler);
+    }
+
+    public void Publish<TEvent>(TEvent @event)
+    {
+        if (_registry.TryGetValue(typeof(TEvent), out var handlers))
+        {
+            foreach (var handler in handlers.OfType<Action<TEvent>>())
+            {
+                handler(@event);
+            }
+
+            foreach (var asyncHandler in handlers.OfType<Func<TEvent, Task>>())
+            {
+                asyncHandler(@event).ConfigureAwait(false);
+            }
+        }
+    }
+
+    public async Task PublishAsync<TEvent>(TEvent @event)
+    {
+        if (_registry.TryGetValue(typeof(TEvent), out var handlers))
+        {
+            var tasks = handlers.OfType<Func<TEvent, Task>>()
+                                .Select(handler => handler(@event))
+                                .ToList();
+
+            foreach (var handler in handlers.OfType<Action<TEvent>>())
+            {
+                handler(@event);
+            }
+
+            await Task.WhenAll(tasks);
+        }
+    }
+
+    public void Unsubscribe<TEvent>(Action<TEvent> handler)
+    {
+        if (_registry.TryGetValue(typeof(TEvent), out var handlers))
+        {
+            handlers.Remove(handler);
+        }
+    }
+
+    public void UnsubscribeAsync<TEvent>(Func<TEvent, Task> handler)
+    {
+        if (_registry.TryGetValue(typeof(TEvent), out var handlers))
+        {
+            handlers.Remove(handler);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using Events;
+
+var eventCentral = EventCentral.Instance;
+
+eventCentral.Subscribe<string>(s => Console.WriteLine($"String event: {s}"));
+
+eventCentral.Publish("Hello, world!");
